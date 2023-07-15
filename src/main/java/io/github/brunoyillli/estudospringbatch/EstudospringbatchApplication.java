7fc7f59622473877a8f181635db59d7a0ad953b4
brunoyillli <wolfteam1910@gmail.com>
2023-07-14 23:39:08 -0300

    estrutura basica projeto

---
+++ src/main/java/io/github/brunoyillli/estudospringbatch/EstudospringbatchApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.estudospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudospringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudospringbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brunoyillli/estudospringbatch/EstudospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.estudospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
