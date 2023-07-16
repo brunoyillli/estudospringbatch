fbfb21f76a1e8c08a2f00f0f09b1f02b98792c91
brunoyillli <wolfteam1910@gmail.com>
2023-07-15 22:37:25 -0300

    refatorada no codigo para dividir ele melhor

--- src/main/java/io/github/brunoyillli/estudospringbatch/step/ImprimeOlaStepConfig.java
+++ src/main/java/io/github/brunoyillli/estudospringbatch/step/ImprimeOlaStepConfig.java
@@ -1,16 +1,12 @@
 package io.github.brunoyillli.estudospringbatch.step;
 
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.brunoyillli.estudospringbatch.tasklet.ImprimeOlaTasklet;
 
 @Configuration
 public class ImprimeOlaStepConfig {
@@ -19,23 +15,12 @@ public class ImprimeOlaStepConfig {
 	private StepBuilderFactory stepBuilderFactory;
 	
 	@Bean
-	public Step imprimeOlaStep(){
+	public Step imprimeOlaStep(ImprimeOlaTasklet imprimeOlaTasklet){
 		return stepBuilderFactory
 				.get("imprimeOlaStep")
-				.tasklet(imprimeOlaTasklet(null))
+				.tasklet(imprimeOlaTasklet.imprimeOlaMethodTasklet(null))
 				.build();
 	}
 	
-	@Bean
-	@StepScope
-	public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
-		return new Tasklet() {
-			
-			@Override
-			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-				System.out.println(String.format("Ola, %s!", nome));
-				return RepeatStatus.FINISHED;
-			}
-		};
-	}
+	
 }
---
+++ src/main/java/io/github/brunoyillli/estudospringbatch/tasklet/ImprimeOlaTasklet.java
@@ -0,0 +1,29 @@
+package io.github.brunoyillli.estudospringbatch.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ImprimeOlaTasklet{
+
+
+    @Bean
+    @StepScope
+	public Tasklet imprimeOlaMethodTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println(String.format("Ola, %s!", nome));
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
+
+}
